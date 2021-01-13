a9d9d1cad804439c96d2ca77fb381ca85102ac73
tumul-vikash <tumulvikash@gmail.com>
2021-01-13 17:39:55 +0530

    login and register design over

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from '../includes/navbar';
+import {NavLink} from 'react-router-dom';
+import '../css/login.css';
 
 export default class login extends Component {
     componentDidMount() {
-        document.body.style.backgroundColor = "#f4f4f8"
+        document.body.style.backgroundColor = "#f4f4f8";
     }
     render() {
         return (
             <div>
-                <Navbar />
-                <h1>Login</h1>
+                <div id="loginmain" >
+                    <Navbar />
+                    <div className="wrapper">
+                        <div id="loginform">
+                            <div class="form-group">
+                                <label for="email">Email address:</label>
+                                <input type="email" className="form-control" placeholder="Enter email" id="email" required/>
+                            </div>
+                            <button onClick={() => console.log('form submitted')} type="submit" className="btn btn-primary btn-block">Login</button> 
+                            <NavLink className="registerlink" to="/register">Not yet registered? Register now</NavLink>
+                        </div>
+                    </div>
+                </div>
+                <footer>
+                    <p>di.com - copyright &copy; 2021 </p>
+                </footer>
             </div>
         )
     }
--- src/containers/register.js
+++ src/containers/register.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from '../includes/navbar';
+import {NavLink} from 'react-router-dom';
+import '../css/register.css';
 
-export default class register extends Component {
+export default class login extends Component {
     componentDidMount() {
-        document.body.style.backgroundColor = "#f4f4f8"
+        document.body.style.backgroundColor = "#f4f4f8";
     }
     render() {
         return (
             <div>
-                <Navbar />
-                <h1>Register</h1>
+                <div id="registermain" >
+                    <Navbar />
+                    <div className="wrapper">
+                        <div id="loginform">
+                            <div class="form-group">
+                                <label for="name">Username:</label>
+                                <input type="text" className="form-control" placeholder="Enter username" id="name" required/>
+                                <label for="email">Email address:</label>
+                                <input type="email" className="form-control" placeholder="Enter email" id="email" />
+                            </div>
+                            <button onClick={() => console.log('form submitted')} type="submit" className="btn btn-primary btn-block">Login</button> 
+                            <NavLink className="registerlink" to="/login">Already registered? Login</NavLink>
+                        </div>
+                    </div>
+                </div>
+                <footer>
+                    <p>di.com - copyright &copy; 2021 </p>
+                </footer>
             </div>
         )
     }
